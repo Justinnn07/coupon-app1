5f35b81282d851f3ae1f2d2918586c60a19e1de7
Justinnn07 <justinv2006@gmail.com>
2022-01-05 11:42:26 +0530

    initial commit

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 const baseUrl = "https://coupon-solicits-1.herokuapp.com";
+const accessToken = "54f2785ab5306a340f186cf23cd00556";
 const config = {
   login_url: baseUrl + "/users/login",
   register_url: baseUrl + "/users/register",
@@ -7,8 +8,7 @@ const config = {
   hotel_url: baseUrl + "/users/hotels",
   advert_url: baseUrl + "/users/advert",
   updateProfile_url: baseUrl + "/users/me",
-  locationApi_url:
-    "http://api.positionstack.com/v1/reverse?access_key=54f2785ab5306a340f186cf23cd00556&query=",
+  locationApi_url: `http://api.positionstack.com/v1/reverse?access_key=${accessToken}&query=`,
 };
 
 export default config;
